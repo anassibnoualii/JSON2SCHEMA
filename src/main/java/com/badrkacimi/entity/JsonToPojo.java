00b682b6de92376089d81d98ab7870b134a7826c
anass ibnou ali <a.ibnouali@insea.ac.ma>
2019-05-22 22:47:16 +0000

    [refactoring] - removing redundent variables

--- src/main/java/com/badrkacimi/entity/JsonToPojo.java
+++ src/main/java/com/badrkacimi/entity/JsonToPojo.java
@@ -22,17 +22,18 @@ public class JsonToPojo {
 
         List<File> files = gettingJsonFiles();
 
-        convertingJsonToPojo(PACKAGE_NAME, OUTPUT_POJO_DIRECTORY, files, INITIAL_INDEX);
+        convertingJsonToPojo(files);
 
     }
 
-    private static void convertingJsonToPojo(String packageName, File outputPojoDirectory, List<File> files, int i) throws MalformedURLException {
+    private static void convertingJsonToPojo( List<File> files) throws MalformedURLException {
+        int i =INITIAL_INDEX;
         for (File file : files) {
             URL inputjson = file.toURI().toURL();
             String className = file.getName();
 
             try {
-                convertToJson(inputjson, outputPojoDirectory, packageName, className.replace(".json", ""));
+                convertToJson(inputjson, className.replace(".json", ""));
                 i++;
 
 
@@ -49,26 +50,25 @@ public class JsonToPojo {
         File dir = new File("./src/main/resources/json/json-files");// input
         String[] extensions = new String[]{"json"};
         System.out.println("Obtenir tous les json... " + dir.getCanonicalPath() + " y compris ceux dans les sous-repertoires");
-        List<File> files = (List<File>) FileUtils.listFiles(dir, extensions, true);
-        return files;
+        return (List<File>) FileUtils.listFiles(dir, extensions, true);
+
     }
 
 
-    private static void convertToJson(URL inputJson, File outputPojoDirectory, String packageName, String className) throws IOException {
+    private static void convertToJson(URL inputJson, String className) throws IOException {
         JCodeModel codeModel = new JCodeModel();
-        URL source = inputJson;
         GenerationConfig config = new DefaultGenerationConfig() {
             @Override
             public boolean isGenerateBuilders() { // set config option by overriding method
                 return true;
             }
-
+            @Override
             public SourceType getSourceType() {
                 return SourceType.JSON;
             }
         };
         SchemaMapper mapper = new SchemaMapper(new RuleFactory(config, new Jackson2Annotator(config), new SchemaStore()), new SchemaGenerator());
-        mapper.generate(codeModel, className, packageName, source);
-        codeModel.build(outputPojoDirectory);
+        mapper.generate(codeModel, className, PACKAGE_NAME, inputJson);
+        codeModel.build(OUTPUT_POJO_DIRECTORY);
     }
 }
